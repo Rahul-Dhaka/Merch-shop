49e4a600ca08732713518fe28479192769d5bb36
“Rahul <dhakarahul16@gmail.com>
2024-07-18 23:09:48 +0530

    -

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -82,7 +82,7 @@ const Services = () => {
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
-              Track Order
+              Track your Order
             </button>
           </div>
         </form>
